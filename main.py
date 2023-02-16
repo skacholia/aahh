78136b74a5dacde48e80f72ce50b1eaa959698de
athensai <122765545+athensai@users.noreply.github.com>
2023-02-15 20:00:55 -0500

    Update and rename Home.py to main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Athens Area Habitat for Humanity 🏠", page_icon=":house:", layout="wide")
+
+st.title("Your AI-powered Marketing Tool")
+st.subheader("A custom app built by **Athens AI**")
+
