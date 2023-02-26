72dea4226919fc1cfe24a62b24cc58d1440b05ad
athensai <122765545+athensai@users.noreply.github.com>
2023-02-26 17:13:49 -0500

    Create Search.py

---
+++ pages/Search.py
@@ -0,0 +1,27 @@
+from openai.embeddings_utils import get_embedding
+from openai.embeddings_utils import cosine_similarity
+import openai
+import pandas as pd
+import ast
+
+openai.api_key = "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"
+df = pd.read_csv("habitat_embed.csv")
+df['embeddings'] = df['embeddings'].apply(lambda x: ast.literal_eval(x))
+
+def get_embedding(text, model="text-embedding-ada-002"):
+    return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
+
+def search_embed(df, description, n):
+    description_embedding = get_embedding(
+        description,
+        model="text-embedding-ada-002"
+    )
+    df["similarity"] = df.embeddings.apply(lambda x: cosine_similarity(x, description_embedding))
+
+    results = (
+        df.sort_values("similarity", ascending=False)
+        .head(n)
+    )
+    
+    return results
+
